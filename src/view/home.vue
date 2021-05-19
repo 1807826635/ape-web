09603e01d024d6b9052f45e3a8c5b64b75a4f6b3
wjl <845493506@qq.com>
2021-05-19 21:54:19 +0800

    first commit

---
+++ src/view/home.vue
@@ -0,0 +1,361 @@
+<template>
+  <div class="wrap">
+    <div class="home-top">
+      <div class="top-img">
+        <div class="min-width padding-top">
+          <div class="top-text-z">全方位赋能艺术培训机构</div>
+          <div class="top-text-q">Help art training institutions grow</div>
+          <div class="yellow-box"></div>
+          <div class="sub-text">猩营销是中国艺术培训行业领先的OMO生源裂变增长专家，
+            通过不断创意的营销理念，为中国艺培行业构建系统性、可持续、规模化的营销解
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="min-width body-box-z">
+      <div class="top-center">
+        <div class="pos">
+          <span>为什么选择猩营销APE MARKETING</span>
+          <img :src="require('@/assets/image/one.png')" class="one-png">
+        </div>
+      </div>
+      <div class="dis-center">
+        <div class="text-box">
+          <p>
+            猩营销（原ST研修院）成立于2014年，
+          </p>
+          <p>
+            一站式招生全案开拓者，
+          </p>
+          <p>
+            从招生、运营及管理全方位赋能艺术培训机构的服务性平台！
+          </p>
+          <p>
+            猩营销由“营销鬼才”舒俊老师发起，
+          </p>
+          <p>
+            联合行业知名主理人共同运营！
+          </p>
+          <p>
+            旨在通过强大的营销创意及一线的经验累积，
+          </p>
+          <p>
+            为培行业的整体升级而不断的努力！
+          </p>
+          <p>猩营销原创的拼多多、点灯吸粉、打卡裂变顿号</p>
+        </div>
+      </div>
+      <div class="dis-center dis-ali mechanism">
+        <div class="ali-item">
+          <div>2000+</div>
+          <div>合作机构customer</div>
+        </div>
+        <div class="ali-item">
+          <div>60万+</div>
+          <div>招生人数students</div>
+        </div>
+        <div class="ali-item">
+          <div>5500万+</div>
+          <div>活动业绩achievements</div>
+        </div>
+        <div class="ali-item">
+          <div>50000+</div>
+          <div>听课学员FANS</div>
+        </div>
+      </div>
+      <div class="dis-center">
+        <div class="value-heart">猩营销核心价值观</div>
+      </div>
+      <img :src="require('@/assets/image/one-for-one.png')" class="one-for">
+      <div class="text-center-q">
+        <p>猩营销相信，只有帮助更多的客户，取得更大的成功</p>
+        <p>成就我们的客户，才能达到我们的目标</p>
+      </div>
+    </div>
+    <div>
+      <div class="problem">
+        <div class="has">
+          <p>您的机构碰到了问题吗？想找我们合作吗？</p>
+          <div class="let-talk">让我们聊聊 LET`S TALK</div>
+        </div>
+      </div>
+      <div class="body-box-z">
+        <div class="top-center">
+          <div class="pos">
+            <span>我们的服务SERVERS</span>
+            <img :src="require('@/assets/image/two.png')" class="one-png">
+          </div>
+          <div class="center-1">
+            目前猩营销服务涵盖街舞、中国舞、拉丁、芭蕾、书法、美术、模特、 早教、英语、跆拳道、乐高、口才、主持等多个素质教育培训领域 根
+          </div>
+        </div>
+      </div>
+      <div class="body-box-z section-1">
+        <div class="to-ground-item">
+          <div class="first-text">落地招生Marketing</div>
+          <div class="yellow-box-1"></div>
+          <div class="text-2">拼多多案</div>
+          <div class="text-2">猩团案</div>
+          <div class="text-2">爆课案</div>
+          <div class="text-2">社群案</div>
+          <div class="text-2">风火轮案</div>
+        </div>
+        <div class="to-ground-item">
+          <div class="first-text">定制内训training</div>
+          <div class="yellow-box-1"></div>
+          <div class="text-2">拼多多案</div>
+          <div class="text-2">猩团案</div>
+          <div class="text-2">爆课案</div>
+          <div class="text-2">社群案</div>
+          <div class="text-2">风火轮案</div>
+        </div>
+        <div class="to-ground-item">
+          <div class="first-text">增长裂变system</div>
+          <div class="yellow-box-1"></div>
+          <div class="text-2">拼多多案</div>
+          <div class="text-2">猩团案</div>
+          <div class="text-2">爆课案</div>
+          <div class="text-2">社群案</div>
+          <div class="text-2">风火轮案</div>
+        </div>
+        <div class="to-ground-item">
+          <div class="first-text">主题峰会training</div>
+          <div class="yellow-box-1"></div>
+          <div class="text-2">拼多多案</div>
+          <div class="text-2">猩团案</div>
+          <div class="text-2">爆课案</div>
+          <div class="text-2">社群案</div>
+          <div class="text-2">风火轮案</div>
+        </div>
+      </div>
+      <div class="body-box-z">
+        <div>
+          <div class="right-now">立即一对一顾问咨询customer service</div>
+        </div>
+      </div>
+      <!--地图-->
+      <div class="body-box-z">
+        <div class="top-center">
+          <div class="pos">
+            <span>我们的客户customer</span>
+            <img :src="require('@/assets/image/three.png')" class="one-png">
+          </div>
+          <div class="center-2">
+
+          </div>
+        </div>
+      </div>
+
+      <div class="section-3">
+        <div class="box-3"></div>
+        <div class="box-3"></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .wrap {
+    .home-top {
+      .top-img {
+        height: 32rem;
+        width: 100%;
+        background: url("../assets/image/home.png");
+        background-size: 100% 100%;
+        object-fit: cover;
+        .top-text-z {
+          color: #ffffff;
+          font-size: 3.4rem;
+        }
+      }
+      .wrap {
+        width: 100%;
+      }
+      .top-text-q {
+        color: #ffffff;
+        font-size: 2.4rem;
+      }
+      .padding-top {
+        padding-top: 5.95rem;
+        padding-left: 3.8rem;
+      }
+      .yellow-box {
+        background: #ffed00;
+        width: 30px;
+        height: 6px;
+        margin-top: 1.8rem;
+        margin-bottom: 1.8rem;
+      }
+      .sub-text {
+        color: #ffffff;
+        font-size: 0.8rem;
+      }
+    }
+    .dis-center {
+      display: flex;
+      width: 100%;
+      justify-content: center;
+    }
+    .text-box {
+      width: 100%;
+      text-align: center;
+      p {
+        color: #181818;
+        font-size: 0.9rem;
+        text-align: center;
+      }
+    }
+    .body-box-z {
+      text-align: center;
+      padding-top: 5.45rem;
+      .top-center {
+        font-size: 1.8rem;
+        width: 100%;
+        text-align: center;
+      }
+      .dis-ali {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        width: 100%;
+        .ali-item {
+          div:first-child {
+            font-size: 3rem;
+            color: #181818;
+            font-weight: 600;
+          }
+          div:last-child {
+            font-size: 0.8rem;
+            color: #181818;
+          }
+        }
+      }
+      .mechanism {
+        margin-top: 2.75rem;
+      }
+      .value-heart {
+        padding: 0.6rem 2.01rem 0.6rem 2.01rem;
+        background: #5e5e5e;
+        display: flex;
+        align-items: center;
+        justify-items: center;
+        color: #ffffff;
+        font-size: 0.8rem;
+        margin-top: 4rem;
+        margin-bottom: 1.75rem;
+      }
+      .one-for {
+        width: 407px;
+      }
+      .text-center-q {
+        text-align: center;
+        font-size: 19px;
+        color: #737373;
+      }
+    }
+    .problem {
+      margin-top: 1rem;
+      background: #f2f2f2;
+      text-align: center;
+      height: 12.25rem;
+      .has {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        p {
+          font-size: 1.5rem;
+          font-weight: 600;
+        }
+      }
+      .let-talk {
+        font-size: 0.8rem;
+        color: #181818;
+        background: #ffed00;
+        border: 1px dashed #181818;
+        padding: 0.6rem 0.85rem;
+        font-weight: bold;
+        cursor: pointer;
+      }
+    }
+    .center-1 {
+      width: 23.4rem;
+      text-align: center;
+      color: #000000;
+      margin: 0 auto;
+      font-size: 0.9rem;
+      line-height: 30px;
+    }
+    .section-1 {
+      display: flex;
+      justify-content: center;
+      .to-ground-item {
+        margin-right: 1.5rem;
+        text-align: left;
+        .first-text {
+          font-size: 1.1rem;
+          color: #000000;
+          font-weight: bold;
+        }
+        .yellow-box-1 {
+          width: 30px;
+          height: 6px;
+          background: #ffed00;
+          margin-top: 0.5rem;
+          margin-bottom: 0.5rem;
+        }
+        .text-2 {
+          margin-bottom: 0.2rem;
+          font-size: 0.8rem;
+          color: #000000;
+        }
+      }
+    }
+    .right-now {
+      padding: 0.6rem 1.6rem;
+      display: inline-block;
+      cursor: pointer;
+      color: #000000;
+      font-size: 0.8rem;
+      border: 1px solid #000000;
+    }
+    .center-2 {
+      width: 40rem;
+      height: 20rem;
+      margin: 0 auto;
+      border: 1px solid #000000;
+    }
+
+    .pos {
+      position: relative;
+      margin-bottom: 2.25rem;
+      .one-png {
+        width: 3.75rem;
+        position: absolute;
+        left: 50%;
+        top: -24px;
+        transform: translateX(-50%);
+        z-index: -1;
+      }
+    }
+    .section-3{
+      display: flex;
+          .box-3{
+            flex: 1;
+            
+          }
+    }
+    .min-width {
+      width: 1400px;
+      margin: 0 auto;
+    }
+    margin-bottom: 40px;
+  }
+</style>
